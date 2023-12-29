a172d4f7242b0cb185c16525249fbf63483296b5
Femina Jamal <git@gitlab.com>
2023-12-29 10:53:00 +0530

    pushing

---
+++ Pages/LoginPage.ts
@@ -0,0 +1,18 @@
+import { Page } from "@playwright/test";
+
+export class LoginPage{
+    readonly page: Page
+
+    constructor(page: Page){
+        this.page = page
+    }
+
+    async testingAuthentication(email: string, password: string){
+        await this.page.locator('#menu-item-1311').click()
+        await this.page.locator('[name="xoo-el-username"]').fill(email)
+        await this.page.locator('[name="xoo-el-password"]').fill(password)
+        await this.page.getByText('Sign in').click()
+
+        const text = await this.page.locator('#menu-item-1314').textContent()
+    }
+}
---
+++ tests/testBitheap.spec.ts
@@ -0,0 +1,15 @@
+import {expect, test} from '@playwright/test'
+import { Page } from '@playwright/test';
+import { LoginPage } from "../Pages/loginPage";
+import { text } from 'stream/consumers';
+
+test.beforeEach(async ({page}) => {
+    await page.goto('https://bitheap.tech')
+})
+
+test('loginTest', async({page}) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.testingAuthentication('playwright', 'playwright')
+
+    expect(text, 'Hello, Playwright')
+})
